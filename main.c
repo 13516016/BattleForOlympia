d84077592a8c84360330dd971d643e3493fc2583
13516016 <31983792+13516016@users.noreply.github.com>
2017-11-23 10:25:38 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+// #include "building.h"
+#include "playerQueue.h"
+#include "buildingList.h"
+#include "printformat.h"
+#include "unitList.h"
+#include "map.h"
+#include "gameplay.h"
+
+int main() {
+
+  Queue Q;
+  Map M;
+  clearScreen();
+  printLogo();
+  newGame(&Q,&M);
+  while (NbElmt(Q) == 2) {
+    printf("%d\n",NbElmt(Q) );
+    playTurn(&Q,&M);
+  }
+  clearScreen();
+  printf("\n====================================================\n" );
+  printf("         Player %d won the game!\n", (InfoHead(Q).ID));
+  printf("\n====================================================\n" );
+
+
+  return 0;
+}
